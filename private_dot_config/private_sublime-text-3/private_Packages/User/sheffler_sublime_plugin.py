146c2ffdab193638b6ddd2c860f0b3a38b504533
Will Sheffler <willsheffler@gmail.com>
2025-03-15 20:39:33 -0700

    sublime last_result plugin and commands file

--- private_dot_config/private_sublime-text-3/private_Packages/User/sheffler_sublime_plugin.py
+++ private_dot_config/private_sublime-text-3/private_Packages/User/sheffler_sublime_plugin.py
@@ -1,7 +1,6 @@
-import sublime
 import sublime_plugin
 
-class MyMoveLineCommand(sublime_plugin.TextCommand):
+class WhsMoveLineCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, lines=10, extend=False):
         # row, col = self.view.rowcol(self.view.sel()[0].begin())
@@ -9,9 +8,21 @@ class MyMoveLineCommand(sublime_plugin.TextCommand):
         for _ in range(lines):
             self.view.run_command("move", {'by': 'lines', 'forward': fwd, 'extend': extend})
 
-class MySelectLinesCommand(sublime_plugin.TextCommand):
+class WhsSelectLinesCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, lines=10):
         fwd, lines = lines > 0, abs(lines)
         for _ in range(lines):
             self.view.run_command("select_lines", {'forward': fwd})
+
+class WhsLastResultCommand(sublime_plugin.TextCommand):
+    def run(self, edit, forward=True, num_steps=10):
+        window = self.view.window()
+        if not window:
+            print("[WhsLastResult] No window found")
+            return
+        for _ in range(num_steps):
+            if forward:
+                window.run_command("next_result")
+            else:
+                window.run_command("prev_result")
